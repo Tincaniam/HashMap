fee8dea36d397798ef62de937b5936a24c112841
Tincaniam <matttinnel@gmail.com>
2022-11-21 20:51:22 -0800

    Update a6_include.py

--- a6_include.py
+++ a6_include.py
@@ -1,9 +1,4 @@
-# Course:      CS261 - Data Structures
-# Assignment:  6
-# Description: Provided data structures necessary to complete the assignment.
-#              Please look through this file carefully to see what methods
-#              are available and how they're implemented.
-#              Don't modify the contents of this file.
+# Custom DynamicArray, SLNode, LinkedList data structures and hash functions.
 
 
 # -------------- Used by both HashMaps (SC & OA)  -------------- #
